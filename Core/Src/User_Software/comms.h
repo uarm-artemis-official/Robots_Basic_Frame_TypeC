b08fdab6646a051ee7aa2c2bbb83540da6cbd44a
tazvik01 <tazvikziauddin@gmail.com>
2024-01-06 20:16:17 -0700

    updated comm task for the 3 newly added keys

--- Core/Src/User_Software/comms.h
+++ Core/Src/User_Software/comms.h
@@ -25,6 +25,7 @@ typedef enum {
 	COMM_PC_CONTROL = 1 << 2,      // 0b00100
     COMM_REFEREE = 1 << 3,         // 0b01000
     COMM_VISION = 1 << 4,          // 0b10000
+	COMM_ADD_PC_CONTROL = 1 << 5,  // 0b100000
 
     							   // add more here if needed
 } CommMessageType_t;
@@ -68,6 +69,11 @@ typedef struct{
 	uint8_t send_flag;
 }CommPCControl_t;
 
+typedef struct{
+	int16_t pc_data[3]; //switch to shoot mode, 180 degree turn around, switch barrel
+	uint8_t send_flag;
+}CommAddPCControl_t;
+
 /* referee info   -> chassis to gimbal */
 typedef struct{
 	game_status_t 	  game_status_data;
@@ -90,6 +96,8 @@ typedef union {
 	CommReferee_t comm_ref;
 	/* vision info    -> chassis to gimbal */
 	CommVision_t vision;
+	/* pc control -> chassis to gimbal */
+	CommAddPCControl_t comm_add_pc;
 }CommMessageUnion_t;
 
 /* Abstract communication module interface */
