62f52f158e2379bd59b64e294c9ebef1cbffd8c6
James Fu <fzjames2037@gmail.com>
2024-03-08 18:17:10 -0700

    Added new auto aim code

--- Core/Src/User_Software/auto_aim.h
+++ Core/Src/User_Software/auto_aim.h
@@ -2,7 +2,8 @@
 * @file           : auto_aim.h
 * @brief          : auto aim
 * @created time	  : Oct, 2023
-* @author         : Haoran Qi
+* @modified time  : Mar, 2024
+* @authors        : Haoran Qi, James Fu
 ******************************************************************************
 * Copyright (c) 2023 UARM Artemis.
 * All rights reserved.
@@ -12,63 +13,68 @@
 #ifndef __AUTO_AIM_H__
 #define __AUTO_AIM_H__
 
-/* define the parse states */
-#define UC_RX_PACKLEN  12 // total length of the packet in bytes
-#define UC_RX_DATALEN  10 // the end of the position of the used data in a pack in bytes
-#define UC_RX_READ_START 1
+#include "stm32f4xx_hal.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
+// PACKET STRUCTS AND DEFINITIONS
+// All packet sizes are in bytes.
 
-
-/* rm_vision open-source pack data structures using CRC16 */
+#define UC_RECV_PACK_HEADER 0x99
+#define UC_RECV_PACK_SIZE 16
 typedef struct{
-  uint8_t header;
-  uint8_t tracking;
-  float x;
-  float y;
-  float z;
-  float yaw;
-  float vx;
-  float vy;
-  float vz;
-  float v_yaw;
-  float r1;
-  float r2;
-  float z_2;
-  uint16_t checksum;
-} RecvPacket_t;
+	uint8_t header;
+	uint8_t target_num;
+	float delta_yaw;
+	float delta_pitch;
+	uint32_t checksum;
+} UC_Recv_Pack_t; //receive packet from upper computer
 
+#define UC_SEND_PACK_HEADER 0x5D
+#define UC_SEND_PACK_SIZE 40
 typedef struct{
-  uint8_t header;
-  uint8_t robot_color : 1; // 0: red, 1: blue
-  uint8_t task_mode : 2; // no use
-  uint8_t reserved : 5;
-  float pitch;
-  float yaw;
-  float aim_x;
-  float aim_y;
-  float aim_z;
-  uint16_t checksum;
-} SendPacket_t;
+	uint8_t header;
+	uint8_t robot_color; // 0: red, 1: blue
+	uint8_t task_mode; // no use
 
-/* UARM rm_vision adapted pack, using checksum calibration*/
-typedef struct{
-	/* header */
-	uint8_t header; // 1 byte
+	// imu data
+	float pitch;
+	float yaw;
+	float accel_x;
+	float accel_y;
 
-	/* auto aiming */
-	float delta_yaw;	   // 4 bytes
-	float delta_pitch;	   // 4 bytes
-	int8_t target_num;    // 1 byte
+	float wheel_rpm[4]; // from chassis wheels
+	uint32_t checksum;
+} UC_Send_Pack_t;
 
-	/* checksum calibration */
-	uint16_t checksum;      // 2 bytes
-}UC_Recv_Pack_t; //receive packet from upper computer
+#define UC_RESPONSE_PACK_HEADER 0x11
+#define UC_RESPONSE_PACK_SIZE 8
+typedef struct {
+	uint8_t header;
+	/* Response code meanings
+	 *  0 - packet received with no errors.
+	 *  1 - packet received with errors.
+	 */
+	uint8_t response_code;
+	uint32_t checksum;
+} UC_Response_Pack_t;
 
-extern UC_Recv_Pack_t uc_rx_pack;
-/* delace the auto aim related functions here */
+
+extern UART_HandleTypeDef huart1;
+uint8_t uc_input_buffer[UC_RECV_PACK_SIZE];
+UC_Recv_Pack_t uc_rx_pack;
+
+
+// INIT FUNCTIONS
 void uc_rx_pack_init(UC_Recv_Pack_t *uc_pack);
-uint16_t calculate_checksum(const char data[], size_t length);
-uint8_t uc_parse_recv_packet(const char recv_pack[], UC_Recv_Pack_t *uc_pack);
+void uc_tx_pack_init(UC_Send_Pack_t *uc_tx_pack);
+void uc_response_pack_init(UC_Response_Pack_t *uc_response_pack);
 
+uint32_t calculate_checksum(void *data, size_t size);
+
+// UC COMMUNICATION FUNCTIONS
+void uc_receive_packet();
+uint8_t uc_send_packet(void* packet, uint16_t packet_size);
 
 #endif /*__AUTO_AIM_H__*/
--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -67,6 +67,8 @@ extern DMA_HandleTypeDef hdma_usart1_rx;
 extern DMA_HandleTypeDef hdma_usart1_tx;
 extern DMA_HandleTypeDef hdma_usart2_rx;
 extern DMA_HandleTypeDef hdma_usart3_rx;
+extern UART_HandleTypeDef huart1;
+extern UART_HandleTypeDef huart2;
 /* USER CODE BEGIN EV */
 extern SPI_HandleTypeDef hspi1;
 extern DMA_HandleTypeDef hdma_spi1_rx;
@@ -290,6 +292,34 @@ void EXTI9_5_IRQHandler(void)
   /* USER CODE BEGIN EXTI9_5_IRQn 1 */
 
   /* USER CODE END EXTI9_5_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART1 global interrupt.
+  */
+void USART1_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART1_IRQn 0 */
+
+  /* USER CODE END USART1_IRQn 0 */
+  HAL_UART_IRQHandler(&huart1);
+  /* USER CODE BEGIN USART1_IRQn 1 */
+
+  /* USER CODE END USART1_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART2 global interrupt.
+  */
+void USART2_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART2_IRQn 0 */
+
+  /* USER CODE END USART2_IRQn 0 */
+  HAL_UART_IRQHandler(&huart2);
+  /* USER CODE BEGIN USART2_IRQn 1 */
+
+  /* USER CODE END USART2_IRQn 1 */
 }
 
 /**
