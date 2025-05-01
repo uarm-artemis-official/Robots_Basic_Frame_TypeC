56b897d8bdf22399dc2e931fee027ff50439d5e7
James Fu <zejia@ualberta.ca>
2025-05-01 14:14:31 -0600

    Add gtest

---
+++ cmake/mingw64-gcc.cmake
@@ -0,0 +1,41 @@
+set(CMAKE_SYSTEM_NAME               Windows)
+set(CMAKE_SYSTEM_PROCESSOR          AMD64)
+
+set(CMAKE_C_COMPILER_ID GNU)
+set(CMAKE_CXX_COMPILER_ID GNU)
+
+# Some default GCC settings
+# arm-none-eabi- must be part of path environment
+# set(TOOLCHAIN_PREFIX                arm-none-eabi-)
+set(TOOLCHAIN_PREFIX C:/msys64/ucrt64/bin/)
+
+set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc.exe)
+set(CMAKE_ASM_COMPILER              ${CMAKE_C_COMPILER})
+set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_PREFIX}g++.exe)
+set(CMAKE_LINKER                    ${TOOLCHAIN_PREFIX}g++.exe)
+set(CMAKE_OBJCOPY                   ${TOOLCHAIN_PREFIX}objcopy.exe)
+set(CMAKE_SIZE                      ${TOOLCHAIN_PREFIX}size.exe)
+
+set(CMAKE_EXECUTABLE_SUFFIX_ASM     ".exe")
+set(CMAKE_EXECUTABLE_SUFFIX_C       ".exe")
+set(CMAKE_EXECUTABLE_SUFFIX_CXX     ".exe")
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+# MCU specific flags
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -x assembler-with-cpp -MMD -MP")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Wpedantic -fdata-sections -ffunction-sections")
+
+set(CMAKE_C_FLAGS_DEBUG "-O0 -g3")
+set(CMAKE_C_FLAGS_RELEASE "-Os -g0")
+set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g3")
+set(CMAKE_CXX_FLAGS_RELEASE "-Os -g0")
+
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
+
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,-Map=${CMAKE_PROJECT_NAME}.map -Wl,--gc-sections")
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--start-group -lm -Wl,--end-group")
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--print-memory-usage")
+
+set(CMAKE_CXX_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--start-group -lstdc++ -lsupc++ -Wl,--end-group")
