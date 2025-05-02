15f6539ce442fe6a628da5bcecd5c4f6e3297e9c
James Fu <zejia@ualberta.ca>
2025-05-02 13:11:43 -0600

    Add uarm_lib

---
+++ Core/Inc/uarm_lib.h
@@ -0,0 +1,15 @@
+#ifndef __UARM_LIB_H
+#define __UARM_LIB_H
+
+#ifdef GTEST
+    #include "assert.h"
+
+    #define ASSERT(cond, msg) assert((cond) && (msg))
+#else
+    #include "error_handler.h"
+
+    #define ASSERT(cond, msg) if (!(cond)) error_handler((msg))
+#endif
+
+
+#endif
