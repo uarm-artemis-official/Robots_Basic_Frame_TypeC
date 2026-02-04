e9f1beeb6830478bcffd934ee2fdbe121225ce3b
James Fu <zejia@ualberta.ca>
2026-02-03 22:46:01 -0700

    Remove DMA from UART1

--- base/Core/Inc/stm32f4xx_it.h
+++ base/Core/Inc/stm32f4xx_it.h
@@ -64,9 +64,7 @@ void TIM8_UP_TIM13_IRQHandler(void);
 void DMA2_Stream1_IRQHandler(void);
 void CAN2_TX_IRQHandler(void);
 void CAN2_RX0_IRQHandler(void);
-void DMA2_Stream5_IRQHandler(void);
 void DMA2_Stream6_IRQHandler(void);
-void DMA2_Stream7_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
